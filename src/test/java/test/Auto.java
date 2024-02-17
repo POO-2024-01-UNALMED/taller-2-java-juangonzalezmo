cf247863ee13ebea9211855118143056d2b108cf
juangonzalezmo <juagonzalezmo@unal.edu.co>
2024-02-16 19:50:08 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -2,17 +2,17 @@ package test;
 
 public class Auto {
 	
-	public String modelo;
-	public int precio;
+	public String modelo = null;
+	public int precio = 0;
 	public Asiento [] asientos;
-	public String marca;
-	public Motor motor;
-	public int registro;
-	public static int cantidadCreados;
+	public String marca = null;
+	public Motor motor = null;
+	public int registro = 0;
+	public static int cantidadCreados = 0;
 	
 	public int cantidadAsientos() {
 		
-		return asientos.length;
+		return this.asientos.length;
 	}
 	
 	public String verificarIntegridad() {
