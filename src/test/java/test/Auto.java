f65d186c3d64ff50dd33a8b5b4cd572cc3068f62
juangonzalezmo <juagonzalezmo@unal.edu.co>
2024-02-16 19:00:10 -0500

    Crean clases
    Se crean las 3 clases del ejercicio con sus atributos y métodos

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,45 @@
+package test;
+
+public class Auto {
+	
+	public String modelo;
+	public int precio;
+	public Asiento [] asientos;
+	public String marca;
+	public Motor motor;
+	public int registro;
+	public static int cantidadCreados;
+	
+	public int cantidadAsientos() {
+		
+		return this.asientos.length;
+	}
+	
+	public String verificarIntegridad() {
+		
+		boolean veracidad=false;
+		for(int i=0; i<this.asientos.length; i++) {
+			
+			if (this.registro==this.motor.registro && this.registro==this.asientos[i].registro) {
+				
+				veracidad = true;
+			}
+			
+			else {
+				
+				veracidad = false;
+				break;
+			}
+		}
+		
+		if (veracidad==true) {
+			
+			return "Auto original";
+		}
+		
+		else {
+			
+			return "Las piezas no son originales";
+		}
+	}
+}
